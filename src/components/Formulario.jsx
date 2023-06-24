a4f404c47b3439a5ade57b12f460c13e91b904b2
Benjamin Cortes <benjamca@hotmail.com>
2023-06-24 13:54:17 -0400

    Terminando graficas con Tailwind

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+
 
 export const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className=" text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {''}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+      <form className="bg-white shadow-md rounded-md py-10 px-5">
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="nombreMascota">Nombre Mascota</label>
+          <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="text" 
+          placeholder="Nombre de la mascota" 
+          id="nombreMascota"/>
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="nombrePropietario">Nombre Propietario</label>
+          <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="text" 
+          placeholder="Nombre del propietario" 
+          id="nombrePropietario"/>
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="email">Email</label>
+          <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="email" 
+          placeholder="Email del propietario" 
+          id="email"/>
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="email">Alta</label>
+          <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+          type="date" 
+          id="alta"/>
+        </div>
+
+        <div className="mb-5">
+          <label className="block text-gray-700 uppercase font-bold" htmlFor="sintomas">Sintomas</label>
+         <textarea
+         id="sintomas"
+         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+         placeholder="Describe los sintomas"
+         />
+        </div>
+
+        <input 
+        type="submit"
+        className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 cursor-pointer transition-all"
+        value="Agregar Paciente"
+        />
+
+      </form>
+    </div>
   )
 }
+
+export default Formulario
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,31 @@
+
+
+export const Paciente = () => {
+  return (
+  
+     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-md">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                    <span className="font-normal normal-case">Hook</span>
+        </p>
+        
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+            <span className="font-normal normal-case">Benjamin</span>
+        </p>
+        
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+            <span className="font-normal normal-case">email@email.cl</span>
+        </p>
+          
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+            <span className="font-normal normal-case">10/2/2023</span>
+        </p>
+          
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+            <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam odit, laboriosam, iste nulla explicabo tempora voluptate necessitatibus iusto consequatur aliquid est ad quasi voluptates dignissimos qui reprehenderit, numquam iure ea.</span>
+        </p>
+    </div>
+    
+  )
+}
+
+export default Paciente
